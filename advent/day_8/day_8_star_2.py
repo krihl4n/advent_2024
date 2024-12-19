aad2133ea7e45a0e0f5f2759dac136acf3b17b87
krihl4n <lkolasa1989@gmail.com>
2024-12-19 01:18:40 +0100

    day 8 star 2

--- advent/day_8/day_8_star_2.py
+++ advent/day_8/day_8_star_2.py
@@ -1,17 +1,54 @@
-file_path = "test_input.txt"
+file_path = "input.txt"
 
 points = [(3, 4), (5, 5)]
 
 def find_antinodes_for(point_1, point_2, size):
-
-
     antinodes = []
+    x1 = point_1[0]
+    y1 = point_1[1]
+    x2 = point_2[0]
+    y2 = point_2[1]
+
+    a = float((y1 - y2) / (x1 - x2))
+    b = float(y1 - x1 * ((y1 - y2) / (x1 - x2)))
+
+    print("a:", a)
+    print("b:", b)
+
+    step = abs(x1 - x2)
+
+    if step > 0:
+        starting_point = x1
+        while starting_point - step >= 0:
+            starting_point -= step
+
+        x = starting_point
+        while x < size:
+            y = round(a * x + b)
+            if 0 <= y < size:
+                antinodes.append((x, y))
+            x += step
+    else:
+        step = abs(y1 - y2)
+        starting_point = y1
+        while starting_point - step >= 0:
+            starting_point -= step
+
+        y = starting_point
+        while y < size:
+            x = (b - y) / a
+            if 0 <= x < size:
+                antinodes.append((x, int(y)))
+            y += step
 
     return antinodes
 
-#print(find_antinodes_for((4, 8),(5, 5), 10)) # expected (1, 3) (7, 6)
+
+# print(find_antinodes_for((4, 8),(5, 5), 10)) # expected (1, 3) (7, 6)
+# print(find_antinodes_for((5, 6), (7, 11), 10))  # expected (1, 3) (7, 6)
 
 antenna_map = []
+
 
 def print_map():
     for r in antenna_map:
@@ -19,6 +56,7 @@ def print_map():
             print(f, end="")
         print()
     print()
+
 
 with open(file_path) as input_file:
     for line in input_file:
@@ -39,20 +77,30 @@ for i, row in enumerate(antenna_map):
             else:
                 antennas_locations[antenna] = [(i, j)]
 
-print(antennas_locations)
-
+# print(antennas_locations)
+for k in antennas_locations.keys():
+    print(k, antennas_locations[k])
 unique_antinodes = []
 
 for antenna in antennas_locations.keys():
     locations = antennas_locations[antenna]
-
-    for idx, point1 in  enumerate(locations):
-        for point2 in locations[idx+1:]:
-            antinodes = find_antinodes_for(point1, point2, size = len(antenna_map))
+    if len(locations) < 2:
+        continue
+    for idx, point1 in enumerate(locations):
+        for point2 in locations[idx + 1:]:
+            antinodes = find_antinodes_for(point1, point2, size=len(antenna_map))
+            print("points:", point1, point2)
             print(antinodes)
+            print()
+            # if len(antinodes) == 2:
+            #     continue
             for a in antinodes:
                 if a not in unique_antinodes:
                     unique_antinodes.append(a)
 
+unique_antinodes.sort()
 print("unique antinodes: ", unique_antinodes)
 print("result", len(unique_antinodes))
+
+print(len(antenna_map))
+print(len(antenna_map[1]))
